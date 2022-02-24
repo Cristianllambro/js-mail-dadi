7e754f9a5ce3a101edd6c27e5322af547a56c5e3
cristianllambro <llambro.cristian1996@gmail.comm>
2022-02-24 15:04:48 +0100

    write script

---
+++ email/script.js
@@ -0,0 +1,14 @@
+let inputEmailUser = document.querySelector('#email-user');
+let arrEmail = ['email@gmail.com', 'email1@gmail.com', 'email2@gmail.com', 
+'email3@gmail.com', 'email4@gmail.com', 'email5@gmail.com'];
+
+console.log(arrEmail);
+
+console.log(inputEmailUser)
+
+for ( let inputUserName = 0; inputUserName <= arrEmail.length -1; 1++) {
+    if (arrEmail[inputEmailUser] = 1)
+        console.log(arrEmail[inputEmailUser])
+    
+    break
+}
