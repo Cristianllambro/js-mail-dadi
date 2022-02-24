9892994edba0ba5ed417cf6cebb7e4505343dfed
cristianllambro <llambro.cristian1996@gmail.comm>
2022-02-24 15:24:54 +0100

    add btn

--- email/script.js
+++ email/script.js
@@ -3,14 +3,13 @@ let btnCheck = document.querySelector('#btn-check')
 let arrEmail = ['email@gmail.com', 'email1@gmail.com', 'email2@gmail.com', 
 'email3@gmail.com', 'email4@gmail.com', 'email5@gmail.com'];
 
-console.log(arrEmail);
-
-console.log(inputEmailUser)
 
 btnCheck.addEventListener('click', function () {
     for ( let inputUserName = 0; inputUserName <= arrEmail.length -1; inputEmailUser++) {
         if (arrEmail[inputEmailUser] = inputEmailUser)
-            console.log(arrEmail[inputEmailUser])
-        
+    
+        document.getElementById('output').innerHTML = `${inputEmailUser}, la tua E-mail e' gia' registrata.`
+    }    
+
 
 })
