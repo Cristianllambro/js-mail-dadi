b26484e6f822f637dc25771cf95c2add7c581f34
cristianllambro <llambro.cristian1996@gmail.comm>
2022-02-24 19:46:18 +0100

    finish dice

--- dadi/script.js
+++ dadi/script.js
@@ -6,20 +6,20 @@ let btnGenerate = document.querySelector('#btn-gen')
 let userNumberCasual = Math.floor(Math.random () * 6 + 1)
 let computerNumberCasual = Math.floor(Math.random () * 6 + 1)
 
-userNumber.innerHTML = 'Hai tirato:' + userNumberCasual;
-computerNumber.innerHTML = 'Il computer ha tirato:' + computerNumberCasual;
+userNumber.innerHTML = 'Hai tirato: ' + userNumberCasual;
+computerNumber.innerHTML = 'Il computer ha tirato: ' + computerNumberCasual;
 
 
 if (userNumberCasual > computerNumberCasual ) {
-    drawAll.innerHTML = 'Hai vinto'
+    drawAll.innerHTML = 'Hai vinto';
 } else if (computerNumberCasual > userNumberCasual) {
-    drawAll.innerHTML = 'Il computer ha vinto'
+    drawAll.innerHTML = 'Il computer ha vinto';
 } else {
-    drawAll.innerHTML = 'Pareggio'
+    drawAll.innerHTML = 'Pareggio';
 }
 
 btnGenerate,addEventListener('click', function () {
-    location.reload()
+    location.reload();
 })
 
 
