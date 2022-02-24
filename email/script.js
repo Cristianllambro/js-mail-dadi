544bab7024d2842a4db7ae7bd2016c5747c5e69c
cristianllambro <llambro.cristian1996@gmail.comm>
2022-02-24 17:47:07 +0100

    finish work

--- email/script.js
+++ email/script.js
@@ -1,15 +1,26 @@
-let inputEmailUser = document.querySelector('#email-user');
 let btnCheck = document.querySelector('#btn-check')
+let btnClear = document.querySelector('#clear')
+let output = document.querySelector('#output')
+
 let arrEmail = ['email@gmail.com', 'email1@gmail.com', 'email2@gmail.com', 
 'email3@gmail.com', 'email4@gmail.com', 'email5@gmail.com'];
 
+let register = false
 
 btnCheck.addEventListener('click', function () {
-    for ( let inputUserName = 0; inputUserName <= arrEmail.length -1; inputEmailUser++) {
-        if (arrEmail[inputEmailUser] = inputEmailUser)
     
-        document.getElementById('output').innerHTML = `${inputEmailUser}, la tua E-mail e' gia' registrata.`
-    }    
+    let inputEmailUser = document.querySelector('#email-user').value;
 
+    for ( let i = 0; i < arrEmail.length; i++) {
+        if (arrEmail[i] == inputEmailUser) {
+            register = true  
+        }
+    }
+
+    if (register) {
+        output.innerHTML = 'La tua E-mail è già registrata'
+    }else {
+        output.innerHTML = 'La tua E-mail non è registrata'
+    }
 
 })
